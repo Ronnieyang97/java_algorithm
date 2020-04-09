84ca9143a7728c212bca739c63657dced1b8af65
Ronnie Yang <Ronnie Yang@shanghai.mintel.ad>
2020-04-09 15:54:38 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        System.out.println(x);
+
+    }
+}
